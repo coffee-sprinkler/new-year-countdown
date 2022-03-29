dbf0416ea64a7c6e38e444b6a90a0e2e2eeabab1
Kopi Sprinkler <dennisduplon.ifliba@gmail.com>
2022-03-29 15:50:14 +0800

    new year countdown

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const MILLISECONDS_IN_A_DAY = 24 * 60 * 60 * 1000;
+const newYear = new Date(`${new Date().getFullYear() + 1}/01/01`);
 const days = document.querySelector('#days'),
   hours = document.querySelector('#hours'),
   minutes = document.querySelector('#minutes'),
   seconds = document.querySelector('#seconds');
 
 const displayTime = () => {
-  const newYear = new Date(`${new Date().getFullYear() + 1}/01/01`);
   const dateToday = new Date();
 
   const diff = Math.abs(newYear - dateToday);
 
+  const time = subtractTime(diff, MILLISECONDS_IN_A_DAY);
+
+  setText(time);
+};
+
+const subtractTime = (diff, MILLISECONDS_IN_A_DAY) => {
   const daysLeftTillNewYear = Math.floor(diff / MILLISECONDS_IN_A_DAY);
   let hoursLeftTillNewYear = Math.floor(
     ((diff % MILLISECONDS_IN_A_DAY) / (1000 * 60 * 60)) % 24
@@ -21,6 +27,20 @@ const displayTime = () => {
     ((diff % MILLISECONDS_IN_A_DAY) / 1000) % 60
   );
 
+  return {
+    daysLeftTillNewYear,
+    hoursLeftTillNewYear,
+    minutesLeftTillNewYear,
+    secondsLeftTillNewYear,
+  };
+};
+
+const setText = ({
+  daysLeftTillNewYear,
+  hoursLeftTillNewYear,
+  minutesLeftTillNewYear,
+  secondsLeftTillNewYear,
+}) => {
   hoursLeftTillNewYear =
     hoursLeftTillNewYear < 10
       ? `0${hoursLeftTillNewYear}`
